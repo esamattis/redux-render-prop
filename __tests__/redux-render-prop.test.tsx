75f09d100631cd80d73b1910557b3a973d4c2aee
Esa-Matti Suuronen <esa-matti@suuronen.org>
2018-06-06 21:22:37 +0300

    Add rendering test

--- __tests__/redux-render-prop.test.tsx
+++ __tests__/redux-render-prop.test.tsx
@@ -77,3 +77,68 @@ test("can use actions", () => {
 
     expect(spyAction).toHaveBeenCalledTimes(1);
 });
+
+test("parent container can cause render prop to render", () => {
+    const initialState = {foo: "bar"};
+
+    const createComponent = makeCreator({
+        prepareState: state => state as typeof initialState,
+        prepareActions: dispatch => ({}),
+    });
+
+    const FooConnect = createComponent({
+        mapState: state => ({mappedFoo: state.foo}),
+    });
+
+    const store = createStore(s => s, initialState);
+
+    class ParentContainer extends React.Component {
+        state = {count: 1};
+
+        increment = () => {
+            this.setState({count: this.state.count + 1});
+        };
+
+        render() {
+            return (
+                <div>
+                    <button data-testid="button" onClick={this.increment}>
+                        inc
+                    </button>
+                    <div data-testid="parent-count-outer">
+                        {this.state.count}
+                    </div>
+                    <FooConnect
+                        render={data => (
+                            <div>
+                                <div data-testid="foo">{data.mappedFoo}</div>
+                                <div data-testid="parent-count-inner">
+                                    {this.state.count}
+                                </div>
+                            </div>
+                        )}
+                    />
+                </div>
+            );
+        }
+    }
+
+    const App = () => (
+        <Provider store={store}>
+            <div>
+                <ParentContainer />
+            </div>
+        </Provider>
+    );
+
+    const rtl = render(<App />);
+
+    const outer = rtl.getByTestId("parent-count-outer");
+    const inner = rtl.getByTestId("parent-count-inner");
+
+    const button = rtl.getByTestId("button");
+    Simulate.click(button);
+
+    expect(outer.innerHTML).toBe("2");
+    expect(inner.innerHTML).toBe("2");
+});
